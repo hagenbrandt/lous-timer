0ed22f0fdc06b2198f98a93d3da8eb46771588f9
Hagen Brandt <h.brandt@steinberg.de>
2021-03-20 00:37:45 +0100

    Feat: Use Button component in timer instead of button tag

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { useState } from 'react'
+import { Button } from "./Button";
 import tiger from '../assets/tiger.png'
 import meat from '../assets/meat.svg'
 
@@ -8,13 +9,21 @@ export const Timer = (): JSX.Element => {
 
     return (
         <section className={'stage'}>
-            <button type={'button'} className={'button'} onClick={() => setIsTigerMoving(!isTigerMoving)}>Start</button>
+            <Button
+                state={isTigerMoving}
+                activeText={'Reset'}
+                passiveText={'Start'}
+                buttonType={'button'}
+                buttonClassActive={'button active'}
+                buttonClassPassive={'button'}
+                onClick={startAnimation}
+            />
             <img id={'meat'} className={'meat'} src={meat} alt={'meat'} />
             <img id={'tiger'} className={!isTigerMoving ? 'tiger' : 'tiger animation'} src={tiger} alt={'tiger'} />
         </section>
     )
 
-    // function startAnimation(): void {
-    //     setIsTigerMoving(!isTigerMoving)
-    // }
+    function startAnimation(): void {
+        setIsTigerMoving(!isTigerMoving)
+    }
 }
