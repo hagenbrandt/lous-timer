4faa4494ba2f49d96980a39fde12dac2d6cd1847
Hagen Brandt <h.brandt@steinberg.de>
2021-03-20 01:24:55 +0100

    Style: Remove button stylings from .css and set them with styled-components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import styled from "styled-components";
 
 type ButtonProps = {
     state: boolean
@@ -12,8 +13,52 @@ type ButtonProps = {
 
 export const Button = (props: ButtonProps): JSX.Element => {
     return (
-        <button className={props.state ? props.buttonClassActive : props.buttonClassPassive} type={props.buttonType} onClick={props.onClick}>
+        <ButtonStyled className={props.state ? props.buttonClassActive : props.buttonClassPassive} type={props.buttonType} onClick={props.onClick}>
             {props.state ? props.activeText : props.passiveText}
-        </button>
+        </ButtonStyled>
     )
 }
+
+const ButtonStyled = styled.button`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%);
+    border: none;
+    background: rgb(255,69,0);
+    color: white;
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-size: 16px;
+    letter-spacing: .175rem;
+    padding: 20px 40px;
+    border-radius: 20px;
+    
+    &:hover {
+        cursor: pointer;
+        background: rgb(220,20,60)
+    }
+    
+    &:active {
+        background: rgb(220,20,60)
+    }
+    
+    &:focus {
+        outline: none;
+    }
+    
+    &.active {
+        background: rgb(220,20,60);
+        
+        &:hover {
+            background: rgb(255,69,0);
+        }
+        
+        &:active {
+            background: rgb(255,69,0);
+        }
+    }
+    
+    @media (min-width: 768px) {
+        top: 30%;
+    }
+`
