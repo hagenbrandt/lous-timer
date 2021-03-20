4abd6914e546cfaee7bdc6167dd8989d1fc79fb8
Hagen Brandt <h.brandt@steinberg.de>
2021-03-21 00:44:46 +0100

    Feat: Add timing features to display minutes and seconds

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,23 +1,33 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
+import styled from "styled-components";
 
 type ClockProps = {
-    state: boolean;
+    state: boolean
+    seconds: number
 }
 
-export const Clock = (props: ClockProps) => {
-    const [seconds, setSeconds] = useState(1800)
+export const Clock = (props: ClockProps): JSX.Element | null => {
+    const time = new Date(props.seconds * 1000)
+    const minutes = time.getUTCMinutes()
+    const seconds = time.getUTCSeconds()
 
-    useEffect(() => {
-        console.log(props.state)
-        if (props.state) {
-            seconds > 0 && setTimeout(() => setSeconds(seconds - 1), 1000)
-        }
-    }, [props.state ,seconds])
-
-    return (
-        <div>
-            {seconds / Math.floor(60)}
-        </div>
-    )
+    if (props.state) {
+        return (
+            <ClockStyled>
+                {minutes}:{seconds === 0 ? '00' : seconds}
+            </ClockStyled>
+        )
+    }
+    return null
 }
+
+const ClockStyled = styled.div`
+  position: absolute;
+  top: 10%;
+  right: 10%;
+  color: rgba(80, 5, 94, .6);
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-size: 40px;
+  letter-spacing: .175rem;
+  font-weight: 600;
+`
